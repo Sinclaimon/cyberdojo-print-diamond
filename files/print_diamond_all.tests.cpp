d709e8136cb1743a3877726d19c6258da84ce579
wA6Zv3 <wA6Zv3@cyber-dojo.org>
2025-05-01 21:58:19 +0000

    11 ran tests, no prediction, got amber

--- files/print_diamond_all.tests.cpp
+++ files/print_diamond_all.tests.cpp
@@ -4,8 +4,9 @@
 
 class DiamondTest : public ::testing::TestWithParam<char> {
     
-    TEST_P(DiamondTest, GeneratesSymmetricDiamond) {
+    TEST(PrintDiamond, HandlesAllUppercaseLetters) {
         for(char c = 'A'; c <= 'Z'; ++c) {
+            SCOPED_TRACE(::testing::Message() << "Testing letter: " << c);
             std::string diamond = print_diamond(c);
             ASSERT_FALSE(diamond.empty()) <<"Failed for letter: " << c;
             
@@ -14,6 +15,8 @@ class DiamondTest : public ::testing::TestWithParam<char> {
             std::vector<std::string> lines;
             std::string line;
             while (std::getline(iss, line)) {
+                while (!line.empty() && line.back() == ' ')
+                    line.pop_back();
                 lines.push_back(line);
             }
             
