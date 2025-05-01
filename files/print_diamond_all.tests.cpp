f783a9a56627cc401b6a367c6ceeab0f00ae1250
wA6Zv3 <wA6Zv3@cyber-dojo.org>
2025-05-01 21:46:55 +0000

    6 ran tests, no prediction, got amber

--- files/print_diamond_all.tests.cpp
+++ files/print_diamond_all.tests.cpp
@@ -5,12 +5,6 @@
 
 class DiamondTest : public ::testing::TestWithParam<char> {
     
-    INSTANTIATE_TEST_SUITE(
-        AllUppercaseLetters,
-        DiamondTest,
-        ::testing::Values(
-            a,b,c,d,e,f,g,h,i)
-    
     TEST(PrintDiamond, HandlesAllUppercaseLetters) {
         for(char c = 'A'; c <= 'Z'; ++c) {
             std::string diamond = print_diamond(c);
